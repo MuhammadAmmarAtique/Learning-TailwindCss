ec5b2bc2c69d1794c2ada781d3f7610e4eec9e1c
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-11-22 19:22:39 +0500

    removed it currently as it was not working

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content:  ["./index.html", "./css/**/*.css", "./js/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
