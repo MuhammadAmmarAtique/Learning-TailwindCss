fdd695576ba0356025e5a04560445fdb16249547
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-11-22 19:14:43 +0500

    installed tailwind css in this project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:  ["./index.html", "./css/**/*.css", "./js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
